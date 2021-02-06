37ea28fb5bb8a0c5856091ed7f69a6bf7ffa29c5
Rafael Chapine <rchapine@gmail.com>
2021-02-05 22:45:59 -0500

    Mongoose Part 2

--- index.js
+++ index.js
@@ -11,17 +11,11 @@ const connect = mongoose.connect(url, {
 connect.then(() => {
   console.log("Connected correctly to server");
 
-  const newCampsite = new Campsite({
+  Campsite.create({
     name: "React Lake Campground",
     description: "test",
-  });
+  })
 
-  newCampsite
-    .save()
-    .then((campsite) => {
-      console.log(campsite);
-      return Campsite.find();
-    })
     .then((campsites) => {
       console.log(campsites);
       return Campsite.deleteMany();
