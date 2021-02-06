106d162f7e5c28e6f42931ae5eda28f9bfdd0d58
Rafael Chapine <rchapine@gmail.com>
2021-02-05 22:54:21 -0500

    Mongoose part 3

--- index.js
+++ index.js
@@ -16,8 +16,32 @@ connect.then(() => {
     description: "test",
   })
 
-    .then((campsites) => {
-      console.log(campsites);
+    .then((campsite) => {
+      console.log(campsite);
+
+      return Campsite.findByIdAndUpdate(
+        campsite._id,
+        {
+          $set: { description: "Updated Test Document" },
+        },
+        {
+          new: true,
+        }
+      );
+    })
+    .then((campsite) => {
+      console.log(campsite);
+
+      campsite.comments.push({
+        rating: 5,
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes",
+      });
+
+      return campsite.save();
+    })
+    .then((campsite) => {
+      console.log(campsite);
       return Campsite.deleteMany();
     })
     .then(() => {
